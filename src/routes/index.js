edb3756b66cad46e7a5af33f1412795ce13769a5
lntrTin0212 <100136208+lntrTin0212@users.noreply.github.com>
2022-06-29 14:42:34 +0700

    setting routes

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
+import { HeaderOnly } from '~/components/Layout';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
